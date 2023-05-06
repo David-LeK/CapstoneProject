35e33964fb6ad97bd7dc117108e910cfaf8a2177
Trần Quốc Huy <103312252+tranquochuy241101@users.noreply.github.com>
2023-05-06 12:53:21 +0700

    Update stanley_mod.py

--- ros_ws/stm32_ws/src/stanley/scripts/stanley_mod.py
+++ ros_ws/stm32_ws/src/stanley/scripts/stanley_mod.py
@@ -9,9 +9,7 @@ from custom_msg.msg import obj_msgs
 from custom_msg.msg import encoder_input_msg, encoder_output_msg
 from custom_msg.msg import gps_msg
 from custom_msg.msg import mpu_msg
-import skfuzzy as fuzz
 import numpy as np
-import matplotlib.pyplot as plt
 
 class StanleyController(object):
     def __init__(self):
@@ -28,16 +26,13 @@ class StanleyController(object):
         self.car_x = 0.0
         self.car_y = 0.0
         self.car_yaw = 0.0
+        self.car_vel = 0.0
         
         self.v_left = 0.0 # m/s (from encoder)
         self.v_right = 0.0 # m/s (from encoder)
-        self.v_fuzzy_left = 0.0 # m/s (from Fuzzy Pi)
-        self.v_fuzzy_right = 0.0 # m/s (from Fuzzy Pi)
         
         self.circumference = math.pi*0.165 # meter
         
-        # self.ref_x = [681392.75,681396.61]
-        # self.ref_y = [1191301.73,1191295.89]
         self.ref_x = [677673.87, 677674.42]
         self.ref_y = [1217604.07,1217604.62]
         self.ref_yaw = [-math.pi/4, 0] # receive in radian
@@ -121,208 +116,73 @@ class StanleyController(object):
             return
 
         # Step 2: Calculate current position of front wheel
-        v_linear = (self.v_left + self.v_right) / 2.0
-        x_wheel = self.car_x + math.cos(self.car_yaw) * self.wheelbase
-        y_wheel = self.car_y + math.sin(self.car_yaw) * self.wheelbase
-        x_deviation = v_linear * math.cos(self.car_yaw) * self.delta_t
-        y_deviation = v_linear * math.sin(self.car_yaw) * self.delta_t
-        x_wheel += x_deviation
-        y_wheel += y_deviation
-        print("Front wheel coords: " + str(x_wheel) + ", " + str(y_wheel))
+        # v_linear = (self.v_left + self.v_right) / 2.0
+        # x_wheel = self.car_x + math.cos(self.car_yaw) * self.wheelbase
+        # y_wheel = self.car_y + math.sin(self.car_yaw) * self.wheelbase
+        # x_deviation = v_linear * math.cos(self.car_yaw) * self.delta_t
+        # y_deviation = v_linear * math.sin(self.car_yaw) * self.delta_t
+        # x_wheel += x_deviation
+        # y_wheel += y_deviation
+        # print("Front wheel coords: " + str(x_wheel) + ", " + str(y_wheel))
 
-        # Step 3: Check if the target radius is less than 0.5m
-        dx = self.ref_x[-1] - x_wheel
-        dy = self.ref_y[-1] - y_wheel
+        # Step 2: Check if reach the endpoint of path
+        dx = self.ref_x[-1] - self.car_x
+        dy = self.ref_y[-1] - self.car_y
         target_radius = math.sqrt(dx*dx + dy*dy)
         print("Target radius: "+ str(target_radius))
         if target_radius < 0.5:
             # Trajectory has been completed, stop the robot
             self.steering_angle = 0.0
+            self.differential_controller()
+            self.ref_x.clear()
+            self.ref_y.clear()
             return
 
-        # Step 4: Determine next waypoint
+        # Step 3: Determine next waypoint
         min_distance = float('inf')
-        j = self.current_path_index
-        for i in range(self.current_path_index, min(self.current_path_index+self.search_offset, len(self.ref_x))):
-            dx = self.ref_x[i] - x_wheel
-            dy = self.ref_y[i] - y_wheel
-            distance = math.sqrt(dx*dx + dy*dy)
-            if distance < 0.1:
-                # Next point reached, increment index
-                j += 1
-            if distance < min_distance:
-                min_distance = distance
-                        
-        print("Distance to nearest point: " + str(min_distance))
+        # j = self.current_path_index
+        dx = self.ref_x[self.current_path_index] - self.car_x
+        dy = self.ref_y[self.current_path_index] - self.car_y
+        distance = math.sqrt(dx*dx + dy*dy)
+        if distance < 0.1:
+            # Next point reached, change to next index  
+            for i in range(self.current_path_index, min(self.current_path_index+self.search_offset, len(self.ref_x))):
+                check_dx = self.ref_x[i] - self.car_x
+                check_dy = self.ref_y[i] - self.car_y
+                check_distance = math.sqrt(check_dx*check_dx + check_dy*check_dy)
+                if check_distance < min_distance:
+                    min_distance = check_distance
+                    self.current_path_index = i
+        # print("Distance to nearest point: " + str(min_distance))
         #Update index
-        self.current_path_index = j
+        # self.current_path_index = j
         print("Current path index: " + str(self.current_path_index))
 
-        # Step 5: Calculate deviation angle and steering angle
+        # Step 4: Calculate deviation angle and steering angle
         e_fa = -(dx*math.cos(self.car_yaw) + dy*math.sin(self.car_yaw))
         theta_e = self.car_yaw - self.ref_yaw[self.current_path_index]
-        v = v_linear
-        theta_d = -math.atan2(self.k * e_fa, v + self.k_soft)
+        # v = 0.3
+        # v = v_linear
+        self.car_vel = (self.v_left + self.v_right)/2
+        theta_d = -math.atan2(self.k * e_fa, self.car_vel + self.k_soft)
         delta = theta_e + theta_d
         print("Delta: " + str(delta))
         if abs(delta) > self.max_steering_angle:
             delta = math.copysign(self.max_steering_angle, delta)
             
         self.steering_angle = delta
-        
-    def fuzzy_pi_controller(self):
-        # Calculate angular error and change in angular error
-        e = self.steering_angle
-        e_dot = (self.steering_angle - self.pre_steering_angle) / self.delta_t
-        self.pre_steering_angle = self.steering_angle
+        self.differential_controller()
 
-        # Normalize input values
-        e_normalized = e / np.pi
-        e_dot_normalized = 6 / np.pi * e_dot
-
-        # Define input and output variables
-        e_range = np.arange(-1, 1.01, 0.01)
-        e_dot_range = np.arange(-1, 1.01, 0.01)
-        u_dot_range = np.arange(-1, 1.01, 0.01)
-
-        # Define fuzzy membership functions for input and output variables
-        e_nb = fuzz.trapmf(e_range, [-2, -1, -0.22, -0.17])
-        e_ns = fuzz.trimf(e_range, [-0.22, -0.11, 0])
-        e_ze = fuzz.trimf(e_range, [-0.11, 0, 0.11])
-        e_ps = fuzz.trimf(e_range, [0, 0.11, 0.22])
-        e_pb = fuzz.trapmf(e_range, [0.17, 0.22, 1, 2])
-
-        e_dot_ne = fuzz.trapmf(e_dot_range, [-2, -1, -0.22, -0.17])
-        e_dot_ze = fuzz.trimf(e_dot_range, [-0.4, 0, 0.4])
-        e_dot_po = fuzz.trapmf(e_dot_range, [0.17, 0.22, 1, 2])
-        
-        u_dot_nb = fuzz.trimf(u_dot_range, [-1, -0.95, -0.8])
-        u_dot_nm = fuzz.trimf(u_dot_range, [-0.95, -0.8, -0.4])
-        u_dot_ns = fuzz.trimf(u_dot_range, [-0.8, -0.4, 0])
-        u_dot_ze = fuzz.trimf(u_dot_range, [-0.4, 0, 0.4])
-        u_dot_ps = fuzz.trimf(u_dot_range, [0, 0.4, 0.8])
-        u_dot_pm = fuzz.trimf(u_dot_range, [0.4, 0.8, 1])
-        u_dot_pb = fuzz.trimf(u_dot_range, [0.8, 1, 1])
-
-        # Fuzzify input variables
-        e_nb_membership = fuzz.interp_membership(e_range, e_nb, e_normalized)
-        e_ns_membership = fuzz.interp_membership(e_range, e_ns, e_normalized)
-        e_ze_membership = fuzz.interp_membership(e_range, e_ze, e_normalized)
-        e_ps_membership = fuzz.interp_membership(e_range, e_ps, e_normalized)
-        e_pb_membership = fuzz.interp_membership(e_range, e_pb, e_normalized)
-
-        e_dot_ne_membership = fuzz.interp_membership(e_dot_range, e_dot_ne, e_dot_normalized)
-        e_dot_ze_membership = fuzz.interp_membership(e_dot_range, e_dot_ze, e_dot_normalized)
-        e_dot_po_membership = fuzz.interp_membership(e_dot_range, e_dot_po, e_dot_normalized)
-        
-        # Apply fuzzy rules
-        # Rule 1: If error is NB and error_dot is NE, then output is NB
-        rule1 = np.fmin(e_nb_membership, e_dot_ne_membership)
-        u_nb_activation = np.fmin(rule1, u_dot_nb)
-
-        # Rule 2: If error is NS and error_dot is NE, then output is NM
-        rule2 = np.fmin(e_ns_membership, e_dot_ne_membership)
-        u_nm_activation = np.fmin(rule2, u_dot_nm)
-
-        # Rule 3: If error is ZE and error_dot is NE, then output is NS
-        rule3 = np.fmin(e_ze_membership, e_dot_ne_membership)
-        u_ns_activation = np.fmin(rule3, u_dot_ns)
-
-        # Rule 4: If error is PS and error_dot is NE, then output is ZE
-        rule4 = np.fmin(e_ps_membership, e_dot_ne_membership)
-        u_ze_activation = np.fmin(rule4, u_dot_ze)
-
-        # Rule 5: If error is PB and error_dot is NE, then output is PS
-        rule5 = np.fmin(e_pb_membership, e_dot_ne_membership)
-        u_ps_activation = np.fmin(rule5, u_dot_ps)
-
-        # Rule 6: If error is NB and error_dot is ZE, then output is NM
-        rule6 = np.fmin(e_nb_membership, e_dot_ze_membership)
-        u_nm_activation_2 = np.fmin(rule6, u_dot_nm)
-
-        # Rule 7: If error is NS and error_dot is ZE, then output is NS
-        rule7 = np.fmin(e_ns_membership, e_dot_ze_membership)
-        u_ns_activation_2 = np.fmin(rule7, u_dot_ns)
-
-        # Rule 8: If error is ZE and error_dot is ZE, then output is ZE
-        rule8 = np.fmin(e_ze_membership, e_dot_ze_membership)
-        u_ze_activation_2 = np.fmax(rule8, u_dot_ze)
-
-        # Rule 9: If error is PS and error_dot is ZE, then output is PS
-        rule9 = np.fmin(e_ps_membership, e_dot_ze_membership)
-        u_ps_activation_2 = np.fmin(rule9, u_dot_ps)
-
-        # Rule 10: If error is PB and error_dot is ZE, then output is PM
-        rule10 = np.fmin(e_pb_membership, e_dot_ze_membership)
-        u_pm_activation = np.fmin(rule10, u_dot_pm)
-
-        # Rule 11: If error is NB and error_dot is PO, then output is NS
-        rule11 = np.fmin(e_nb_membership, e_dot_po_membership)
-        u_ns_activation_3 = np.fmin(rule11, u_dot_ns)
-
-        # Rule 12: If error is NS and error_dot is PO, then output is ZE
-        rule12 = np.fmin(e_ns_membership, e_dot_po_membership)
-        u_ze_activation_3 = np.fmin(rule12, u_dot_ze)
-
-        # Rule 13: If error is ZE and error_dot is PO, then output is PS
-        rule13 = np.fmin(e_ze_membership, e_dot_po_membership)
-        u_ps_activation_3 = np.fmin(rule13, u_dot_ps)
-
-        # Rule 14: If error is PS and error_dot is PO, then output is PM
-        rule14 = np.fmin(e_ps_membership, e_dot_po_membership)
-        u_pm_activation_2 = np.fmin(rule14,u_dot_pm)
-        
-        #Rule 15: If error is PB and error_dot is PO, then output is PB
-        rule15 = np.fmin(e_pb_membership, e_dot_po_membership)
-        u_pb_activation = np.fmin(rule15, u_dot_pb)
-
-        # Aggregate all the output membership functions
-        aggregated = np.fmax.reduce([u_nb_activation, u_nm_activation, u_ns_activation,
-                                     u_ze_activation, u_ps_activation, u_pm_activation,
-                                     u_ns_activation_2, u_ps_activation_2, u_pm_activation_2, 
-                                     u_nm_activation_2, u_ns_activation_3, u_ze_activation_2, 
-                                     u_ps_activation_3, u_ze_activation_3, u_pb_activation])
-        
-        # Defuzzify to get output
-        u_dot = fuzz.defuzz(u_dot_range, aggregated, 'centroid')
-        self.u = self.pre_u + u_dot*self.delta_t
-        self.pre_u = self.u
-        
-        print("Fuzzy controller output: " + str(self.u))
-        
-        #Plot the membership functions and the aggregated output membership function
-        # fig, ax0 = plt.subplots(figsize=(8, 3))
-        # ax0.plot(u_dot_range, u_dot_nb, 'b', linewidth=1.5, label='NB')
-        # ax0.plot(u_dot_range, u_dot_nm, 'g', linewidth=1.5, label='NM')
-        # ax0.plot(u_dot_range, u_dot_ns, 'r', linewidth=1.5, label='NS')
-        # ax0.plot(u_dot_range, u_dot_ze, 'm', linewidth=1.5, label='ZE')
-        # ax0.plot(u_dot_range, u_dot_ps, 'y', linewidth=1.5, label='PS')
-        # ax0.plot(u_dot_range, u_dot_pm, 'k', linewidth=1.5, label='PM')
-        # ax0.fill_between(u_dot_range, u_ns_activation, facecolor='r', alpha=0.2)
-        # ax0.plot(u_dot_range, u_ns_activation, 'r', linestyle='--', linewidth=0.5)
-        # ax0.fill_between(u_dot_range, u_ze_activation, facecolor='m', alpha=0.2)
-        # ax0.plot(u_dot_range, u_ze_activation, 'm', linestyle='--', linewidth=0.5)
-        # ax0.fill_between(u_dot_range, u_ps_activation, facecolor='y', alpha=0.2)
-        # ax0.plot(u_dot_range, u_ps_activation, 'y', linestyle='--', linewidth=0.5)
-        # ax0.plot([u_dot, u_dot], [0, 1], 'k', linewidth=1.5, alpha=0.9)
-        # ax0.legend()
-
-        # plt.tight_layout()
-        # plt.show()
-        
-        if self.u >= 0:
-            self.v_fuzzy_left = (1 + self.u) * self.v_left
-            self.v_fuzzy_right = (1 - self.u) * self.v_right
-        else:
-            self.v_fuzzy_left = (1 - self.u) * self.v_left
-            self.v_fuzzy_right = (1 + self.u) * self.v_right
-            
-        print("Left motor fuzzy speed: " + str(self.v_fuzzy_left))
-        print("Right motor fuzzy speed: " + str(self.v_fuzzy_right))
-        
+    def differential_controller(self):
         # Publish cmd_vel
-        self.cmd_vel.input_setpoint_m1 = self.v_fuzzy_left*120
-        self.cmd_vel.input_setpoint_m2 = self.v_fuzzy_right*120
+        L = 0.385
+        
+        w = (2 * self.car_vel * math.tan(self.steering_angle)) / L
+        self.v_set_left = (2*self.car_vel - w*L)/2
+        self.v_set_right = (2*self.car_vel + w*L)/2
+
+        self.cmd_vel.input_setpoint_m1 = self.v_set_left*120
+        self.cmd_vel.input_setpoint_m2 = self.v_set_right*120
         self.cmd_vel_pub.publish(self.cmd_vel)
         
 
@@ -337,7 +197,6 @@ class StanleyController(object):
                 print("*************************")
                 self.calculate_steering_angle()
                 print("Steering angle: " + str(self.steering_angle))
-                self.fuzzy_pi_controller()
                 rate.sleep()
 
 if __name__ == '__main__':
