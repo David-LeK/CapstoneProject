0059926613e63dee4a60d627f068cb9ceaf0dc8a
David-LeK <85050945+David-LeK@users.noreply.github.com>
2023-05-16 17:07:29 +0700

    Update run_manual.sh

--- ros_ws/stm32_ws/run_manual.sh
+++ ros_ws/stm32_ws/run_manual.sh
@@ -1,2 +1,2 @@
-python2 /opt/ros/melodic/lib/rosserial_python/serial_node.py _port:=/dev/ttyUSB1 _baud:=57600 &
+python2 /opt/ros/melodic/lib/rosserial_python/serial_node.py _port:=/dev/stm_usb _baud:=57600 &
 roslaunch manual_control manual.launch
