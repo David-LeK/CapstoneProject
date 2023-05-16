2832bdc06c1fe9efdc464cf3f79cd98503abf39a
Huy <tranquochuy241101@gmail.com>
2023-05-16 19:18:47 +0700

    Update

--- ros_ws/catkin_ws/build/camera_processing/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/huy/catkin_ws/src/camera_processing
-# Build directory: /home/huy/catkin_ws/build/camera_processing
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
